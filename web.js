db577fe53ec4651fdd3fe251433a4b218f22fa9c
Dagur Eyjolfsson <day@day.is>
2013-07-21 16:33:02 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buffer = new Buffer(25);
+buffer = fs.readFileSync("index.html", "utf-8");
+var string1 = buffer.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  response.send(string1);
+
 });
 
 var port = process.env.PORT || 5000;
