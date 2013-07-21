a9f4c09147e3cb1aeab77d01ed2994fa85d4af8f
Dagur Eyjolfsson <day@day.is>
2013-07-21 16:40:37 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -2,13 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var buffer = new Buffer(25);
-buffer = fs.readFileSync("index.html", "utf-8");
-var string1 = buffer.toString();
-
 app.get('/', function(request, response) {
-
-  response.send(string1);
+var buf = new Buffer(fs.readFileSync('file.html'), 'utf-8'); 
+response.send(buf.toString());
+//  response.send('Hello World');
 
 });
 
